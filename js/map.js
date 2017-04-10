969ec22136545c393168a4c60e0f3a0bafa5d865
Priest4612 <prie4612@gmail.con>
2017-04-10 17:57:33 +0400

    #11 Личный проект: подробности

--- js/map.js
+++ js/map.js
@@ -1,5 +1,16 @@
 'use strict';
 
-var listHotels = window.generateListHotels();
-window.renderElements.renderPins(listHotels);
-window.renderElements.renderOfferDialog(listHotels);
+(function () {
+  var clsHidden = 'hidden';
+  var offerDialog = document.querySelector('#offer-dialog');
+  var dialogClose = offerDialog.querySelector('.dialog__close');
+  window.openCloseHandler.closeDialog(offerDialog, dialogClose, clsHidden);
+
+  var listHotels = window.generateListHotels();
+  window.renderElements.renderPins(listHotels);
+
+  var pins = document.querySelectorAll('.pin');
+  for (var i = pins.length; i--;) {
+    window.openCloseHandler.openDialog(offerDialog, pins[i], clsHidden, listHotels);
+  }
+})();
---
+++ js/openCloseHandler.js
@@ -0,0 +1,79 @@
+'use strict';
+
+window.openCloseHandler = (function () {
+  var module = {};
+
+  var cssClassActive = 'pin--active';
+  var currentActivePin;
+
+  var isKeydownHandlerEsc = function (evt, element, cssClass) {
+    if (window.utils.isDeactivateEvent(evt)) {
+      closeUserDialog(element, cssClass);
+    }
+  };
+
+  var closeUserDialog = function (element, cssClass) {
+    element.classList.add(cssClass);
+    currentActivePin.classList.remove(cssClassActive);
+    document.removeEventListener('keydown', function (evt) {
+      isKeydownHandlerEsc(evt, element, cssClass);
+    });
+  };
+
+  module.closeDialog = function (element, activator, cssClass) {
+    activator.addEventListener('click', function () {
+      closeUserDialog(element, cssClass);
+    });
+    activator.addEventListener('keydown', function (evt) {
+      if (window.utils.isActivateEvent(evt)) {
+        closeUserDialog(element, cssClass);
+      }
+    });
+  };
+
+
+  var openUserDialog = function (element, cssClass) {
+    element.classList.remove(cssClass);
+    document.addEventListener('keydown', function (evt) {
+      isKeydownHandlerEsc(evt, element, cssClass);
+    });
+  };
+
+  var renderUserDialog = function (activator, arr) {
+    var index = 0;
+
+    // console.log(image.src.slice(2, -4));
+    for (var i = 0; i < arr.length; i++) {
+      if (activator.style.left === (arr[i].location.x + 'px') && activator.style.top === (arr[i].location.y + 'px')) {
+        index = i;
+      }
+    }
+    return arr[index];
+  };
+
+  var isActivePin = function (activator) {
+    if (currentActivePin) {
+      currentActivePin.classList.remove(cssClassActive);
+    }
+    currentActivePin = activator;
+    activator.classList.add(cssClassActive);
+  };
+
+  module.openDialog = function (element, activator, cssClass, listHotels) {
+    var hotel = renderUserDialog(activator, listHotels);
+    activator.addEventListener('click', function () {
+      isActivePin(activator);
+      window.renderElements.renderOfferDialog(hotel);
+      openUserDialog(element, cssClass);
+    });
+    activator.addEventListener('keydown', function (evt) {
+      if (window.utils.isActivateEvent(evt)) {
+        isActivePin(activator);
+        window.renderElements.renderOfferDialog(hotel);
+        openUserDialog(element, cssClass);
+      }
+    });
+  };
+
+  return module;
+})();
